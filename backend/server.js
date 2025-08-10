866615adf9153fc540d35d9809ffa1d2d4c8b3c7
Diatoufall <diatoufall278@gmail.com>
2025-08-10 00:38:03 -0400

    modification

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,9 @@
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-
 const app = express();
-app.use(cors());
-app.use(bodyParser.json());
 
 app.get('/api', (req, res) => {
   res.send('API Backend en marche');
 });
-
-const { router: usersRouter } = require('./usersController');
-app.use('/users', usersRouter);
 
 app.listen(3000, () => {
   console.log('Backend démarré sur http://localhost:3000');
