741a49d677d1bb070a4e31510fd39c759607d559
Diatoufall <diatoufall278@gmail.com>
2025-08-10 21:33:48 -0400

    service AuthService pour gestion des requêtes backend

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,20 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class AuthService {
-  private apiUrl = 'http://localhost:3000';
+  private apiUrl = 'http://localhost:4000/users';
 
   constructor(private http: HttpClient) {}
 
-  register(username: string, password: string): Observable<any> {
+  register(username: string, password: string) {
     return this.http.post(`${this.apiUrl}/register`, { username, password });
   }
 
-  login(username: string, password: string): Observable<any> {
-    return this.http.post(`${this.apiUrl}/login`, { username, password });
+  login(username: string, password: string) {
+    return this.http.post<{ token: string }>(`${this.apiUrl}/login`, { username, password });
+  }
+
+  saveToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  getToken() {
+    return localStorage.getItem('token');
   }
 }
