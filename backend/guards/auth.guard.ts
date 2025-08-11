97b2a293f8a179ac7eb8fe1c8a330a857d8f8764
Diatoufall <diatoufall278@gmail.com>
2025-08-10 22:10:56 -0400

    Creation des fichiers

--- backend/guards/auth.guard.ts
+++ backend/guards/auth.guard.ts
@@ -1,5 +1,27 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  canActivate(): Observable<boolean> {
+    return this.authService.isLoggedIn().pipe(
+      take(1),
+      map((isLoggedIn: boolean) => {
+        if (isLoggedIn) {
+          return true;
+        } else {
+          this.router.navigate(['/login']);
+          return false;
+        }
+      })
+    );
+  }
+}
